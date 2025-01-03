4fc39232f4a2eac222d77d2f444e4e6a06338c21
Natalia Gutierrez <natalia.gutierrez@sofka.com.co>
2025-01-03 15:32:45 -0300

    fix create operation logic

--- infrastructure/entry-points/web/src/main/java/com/bank/handler/OperationHandler.java
+++ infrastructure/entry-points/web/src/main/java/com/bank/handler/OperationHandler.java
@@ -1,6 +1,9 @@
 package com.bank.handler;
 
+import com.bank.Account;
 import com.bank.Operation;
+import com.bank.OperationType;
+import com.bank.appservice.account.FindAccountByIdUseCase;
 import com.bank.data.OperationRequestDTO;
 import com.bank.data.OperationResponseDTO;
 import com.bank.mapper.OperationMapper;
@@ -13,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
 import java.util.NoSuchElementException;
 
 @Component
@@ -24,14 +28,18 @@ public class OperationHandler {
 
     private final CreateOperationUseCase createOperationUseCase;
 
+    private final FindAccountByIdUseCase findAccountByIdUseCase;
+
     public OperationHandler(
             FindAllOperationUseCase findAllOperationUseCase,
             FindOperationByIdUseCase findOperationByIdUseCase,
-            CreateOperationUseCase createOperationUseCase
+            CreateOperationUseCase createOperationUseCase,
+            FindAccountByIdUseCase findAccountByIdUseCase
     ) {
         this.findAllOperationUseCase = findAllOperationUseCase;
         this.findOperationByIdUseCase = findOperationByIdUseCase;
         this.createOperationUseCase = createOperationUseCase;
+        this.findAccountByIdUseCase = findAccountByIdUseCase;
     }
 
     public Flux<OperationResponseDTO> findAllOperations() {
@@ -50,8 +58,30 @@ public class OperationHandler {
     }
 
     public Mono<OperationResponseDTO> createOperation(@Valid OperationRequestDTO operationRequestDTO) {
-        Operation op = OperationMapper.toEntity(operationRequestDTO);
-        return createOperationUseCase.apply(op)
-                .map(OperationMapper::toDTO);
+        return findAccountByIdUseCase.apply(operationRequestDTO.getAccountId())
+                .flatMap(acc -> {
+                    var op = applyOperation(OperationMapper.toEntity(operationRequestDTO, acc));
+                    return createOperationUseCase.apply(op);
+                })
+                .map(OperationMapper::toDTO)
+                .switchIfEmpty(Mono.error(new NoSuchElementException("Account not found.")));
+    }
+
+    private Operation applyOperation(Operation op) {
+        Account account = op.getAccount();
+        OperationType opType = Operation.getOperationTypeObj(op.getType());
+        BigDecimal cost = opType.cost();
+        BigDecimal value = op.getValue();
+
+        BigDecimal operationTotal;
+        switch (opType.action()) {
+            case DEBIT -> operationTotal = value.add(cost).negate();
+            case CREDIT -> operationTotal = value.subtract(cost);
+            default -> operationTotal = BigDecimal.ZERO;
+        }
+
+        account.setBalance(account.getBalance().add(operationTotal));
+
+        return op;
     }
 }
